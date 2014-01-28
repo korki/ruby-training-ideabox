b4d2e85b05e75145bc48f59bbaafd47bb3f236ef
Orest Hrycyna <orest.hrycyna@surfdome.com>
2014-01-28 11:53:14 +0000

    Basic ideabox structure and ideas sorting

---
+++ lib/ideabox/idea.rb
@@ -0,0 +1,18 @@
+class Idea
+
+  attr_reader :title, :description, :rank
+
+  def initialize(title, description)
+    @title = title
+    @description = description
+    @rank = 0
+  end
+
+  def like!
+    @rank += 1
+  end
+
+  def <=>(other)
+    rank <=> other.rank
+  end
+end
---
+++ spec/ideabox/idea_spec.rb
@@ -0,0 +1,35 @@
+require './lib/ideabox/idea'
+
+describe Idea do
+  
+  it "has basic attributes" do
+    idea = Idea.new("title", "description")
+    expect(idea.title).to eq("title")
+    expect(idea.description).to eq("description")
+  end
+
+  it "is likeable" do
+    idea = Idea.new("diet", "carrots and cucumbers")
+    expect(idea.rank).to eq(0)
+    idea.like!
+    expect(idea.rank).to eq(1)
+    idea.like!
+    expect(idea.rank).to eq(2)
+  end
+
+  it "sorts by rank" do
+    diet = Idea.new("diet", "cabbage soup")
+    exercise = Idea.new("exercise", "long distance running")
+    drink = Idea.new("drink", "carrot smoothy")
+
+    exercise.like!
+    exercise.like!
+    drink.like!
+
+    ideas = [drink, exercise, diet]
+
+    expect(ideas.sort).to eq([diet, drink, exercise])
+  end
+
+      
+end
